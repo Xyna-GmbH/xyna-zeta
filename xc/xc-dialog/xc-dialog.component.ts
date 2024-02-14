4dbcb1c73efae65039ba5f84862867f212ca30ba
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2024-02-14 14:53:01 +0100

    Migrating to MDC-based Angular Material Components: Dialog

--- xc/xc-dialog/xc-dialog.component.ts
+++ xc/xc-dialog/xc-dialog.component.ts
@@ -17,7 +17,7 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 import { Component, HostListener, InjectionToken, Injector, Optional } from '@angular/core';
-import { MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialogRef as MatDialogRef } from '@angular/material/legacy-dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { Observable } from 'rxjs';
 
